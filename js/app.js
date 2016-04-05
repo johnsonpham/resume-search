ae6f0a47d3d86d510fee975e95116c0d49f470e9
Phuong H <phuonghqh@gmail.com>
2016-04-05 11:03:48 +0700

    Order language proficiency

--- js/app.js
+++ js/app.js
@@ -13,11 +13,15 @@ $(document).ready(function () {
     hitsPerPage: 20,
     maxValuesPerFacet: 5,
     facets: ['type'],
-    disjunctiveFacets: ['category_en', 'location_en', 'job_level_en', 'most_recent_employer', 'suggested_salary', "updated_date", 'exp_years_en', 'attached'],
+    disjunctiveFacets: ['category_en', 'location_en', 'job_level_en', 'most_recent_employer',
+						'suggested_salary', "updated_date",'exp_years_en', 'attached',
+						'nationality_en','language1_name','language1_proficiency_en'],
     // numericFilters: 'updated_date>=1422359939'
   };
   var FACETS_SLIDER = ["suggested_salary", "updated_date"];
-  var FACETS_ORDER_OF_DISPLAY = ['category_en', 'location_en', 'job_level_en', 'most_recent_employer', 'suggested_salary', "updated_date", 'exp_years_en', 'attached'];
+  var FACETS_ORDER_OF_DISPLAY = ['category_en', 'location_en', 'job_level_en', 'most_recent_employer', 'suggested_salary',
+								'updated_date','exp_years_en', 'attached',
+								'nationality_en','language1_name','language1_proficiency_en'];
   var FACETS_LABELS = {
     category_en: 'Category',
     'location_en': 'Location',
@@ -26,7 +30,10 @@ $(document).ready(function () {
     suggested_salary: 'Suggested Salary',
     updated_date: "Last Modified",
     exp_years_en: 'Years of Experience',
-    attached: 'Resume Type'
+    attached: 'Resume Type',
+	nationality_en: 'Nationality',
+	language1_name: 'Language',
+	language1_proficiency_en: 'Language Proficiency'
   };
 
   var sliders = {};
@@ -172,7 +179,8 @@ $(document).ready(function () {
             (v.name == "true") && (v.label = "Attached");
           });
         }
-        if (facetContent.facet == "exp_years_en" || facetContent.facet == "job_level_en") {//custom code
+        if (facetContent.facet == "exp_years_en" || facetContent.facet == "job_level_en" ||
+          "language1_proficiency_en") {//custom code
           var weights = {
             "15+ years": 1,
             "10-15 years": 2,
@@ -190,7 +198,12 @@ $(document).ready(function () {
             "Manager": 6,
             "Team Leader/Supervisor": 7,
             "Experienced (Non-Manager)": 8,
-            "New Grad/Entry Level/Internship": 9
+            "New Grad/Entry Level/Internship": 9,
+
+            "Native": 1,
+            "Advanced": 2,
+            "Intermediate": 3,
+            "Beginner": 4,
           };
           facetContent.values.forEach(function (v) {
             v.weight = weights[v.name];
@@ -198,7 +211,7 @@ $(document).ready(function () {
           facetContent.values = facetContent.values.sort(function (a, b) {return a.weight - b.weight;});
         }
 
-        _.chain(facetContent.values)
+        //_.chain(facetContent.values)
         facetsHtml += facetTemplate.render(facetContent);
       }
     }
@@ -368,29 +381,6 @@ $(document).ready(function () {
     $searchInput.val('').focus();
     algoliaHelper.setQuery('').clearRefinements().search();
   });
-  // Added by Ninh
-  // $lastModified.on('change', function (e) {
-  //   console.log('lastModified on change');
-  //   //e.preventDefault();
-  //
-  //   //facetName = 'updated_date';
-  //   lastModifiedSelectVal = parseInt($("#last-modified option:selected").val());
-  //   //alert($lastModifiedSelectVal);
-  //   if (lastModifiedSelectVal > -1) {
-  //     lastModified4Search = ($.now() / 1000) - (lastModifiedSelectVal * 24 * 3600);
-  //     //algoliaHelper.removeNumericFilters('updated_date', '>=');
-  //     algoliaHelper.numericFilters = 'updated_date >= ' + lastModified4Search;
-  //   }
-  //   else {
-  //     //alert('Not search by date'+PARAMS.numericFilters);
-  //     //algoliaHelper.removeNumericRefinement('updated_date', '>=');
-  //     algoliaHelper.numericFilters = '';
-  //   }
-  //   setURLParams();
-  //   algoliaHelper.search();
-  //   console.log('lastModified on change ended');
-  // });
-
 
   // URL MANAGEMENT
   // ==============
@@ -447,7 +437,7 @@ $(document).ready(function () {
 /// TOOLTIP
   function tooltip() {
     $('[data-toggle="tooltip"]').tooltip({html: true});
-  };
+  }
   setTimeout(tooltip, 1000);
 
 });
