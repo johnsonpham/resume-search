2662d67d13c6b619af07416b49abe02ad196a222
arecavn <ninhsh@gmail.com>
2016-04-11 10:36:14 +0700

    Update config.js

--- conf/config.js
+++ conf/config.js
@@ -1,5 +1,5 @@
-var APPLICATION_ID = 'JF8Q26WWUD';
-var SEARCH_ONLY_API_KEY = '5977d89c4ecec436493b406a577593ee';
-var INDEX_NAME = 'resume_staging';
+var APPLICATION_ID = '';
+var SEARCH_ONLY_API_KEY = '';
+var INDEX_NAME = '';
 var RESUME_DETAIL_LINK_PREFIX= "http://employer.vietnamworks.com/v2/resume/detail/";
 
