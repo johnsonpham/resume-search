0ecf84feb308bf01982eab928a91f01da36a5b1e
johnsonpham <phamthanhtrinh.vt@gmail.com>
2016-04-11 10:26:10 +0700

    if the salary is 0, it will be display "Negotiable"

--- conf/config.js
+++ conf/config.js
@@ -1,5 +1,5 @@
 var APPLICATION_ID = 'JF8Q26WWUD';
-var SEARCH_ONLY_API_KEY = '';
-var INDEX_NAME = 'resume';
+var SEARCH_ONLY_API_KEY = '5977d89c4ecec436493b406a577593ee';
+var INDEX_NAME = 'resume_staging';
 var RESUME_DETAIL_LINK_PREFIX= "http://employer.vietnamworks.com/v2/resume/detail/";
 
